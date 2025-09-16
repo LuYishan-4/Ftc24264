d6b4d18b2d46ae6046555ce85e4e49586e37ef7d
LuYishan <148339075+LuYishan-4@users.noreply.github.com>
2025-09-16 12:41:18 +0800

    Update Teleop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/Teleop.java
@@ -10,13 +10,14 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.common.commandbase.drivetrain;
 import org.firstinspires.ftc.teamcode.common.BOt;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
-import com.pedropathing.follower.Follower;
+
+
 
 
 @TeleOp(name = "Teleop")
 public class Teleop extends CommandOpMode {
 
-    private Follower follower;
+
 
 
     GamepadEx gamePad1;
@@ -25,9 +26,7 @@ public class Teleop extends CommandOpMode {
 
     @Override
     public void initialize() {
-        CommandScheduler.getInstance().reset();
         gamePad1 = new GamepadEx(gamepad1);
-        follower = Constants.createFollower(hardwareMap);
         BOt.getInstance().initHardware(hardwareMap, telemetry, gamePad1, gamePad1.getLeftX(), gamePad1.getLeftY(), gamePad1.getRightX());
         Command drive = new drivetrain(BOt.getInstance().driveTrain, gamePad1.getLeftX(), gamePad1.getLeftY(), gamePad1.getRightX());
         BOt.getInstance().driveTrain.setDefaultCommand(drive);
@@ -39,3 +38,4 @@ public class Teleop extends CommandOpMode {
             telemetry.update();
         }
     }
+
